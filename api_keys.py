f603cfa271da0048ec1aa5c37744cd81a90f1484
echojniak <ewelina_nwk@yahoo.com>
2024-06-03 08:36:06 -0500

    message

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "77086356f6feed8f477d69d5d7d74744"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "862381c2b5674d0cada10866db2b8e5d"
